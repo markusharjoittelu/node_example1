2e61fa40b5c9cb5438733fe95945a0d0ff6b94e7
Markus <niiranen.markus@gmail.com>
2022-10-26 10:44:42 +0300

    a2 ja a3 valmiit

---
+++ a2.js
@@ -0,0 +1,19 @@
+let x = 0;
+const y = 0;
+var z = 0;
+
+function setValues() {
+    x = 5;
+   // y = 6;
+    z = 7;
+}
+
+function printValues() {
+    console.log("x = " + x)
+    console.log("y = " + y)
+    console.log("z = " + z)
+
+}
+
+setValues();
+printValues();
---
+++ a3.js
@@ -0,0 +1,10 @@
+for (var x = 1; x < 5; x++) {
+    console.log("x arvo = " + x);
+}
+
+for (let y = 1; y < 5; y++) {
+    console.log("y arvo = " + y);
+}
+
+console.log("Loopin jälkeen x = " + x);
+//console.log("Loopin jälkeen y = " + y);
