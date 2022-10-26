74ad4768cbce4d4143b21228ff0226f22cbe413c
Markus <niiranen.markus@gmail.com>
2022-10-26 11:40:22 +0300

    a5 kesken

---
+++ a5.js
@@ -0,0 +1,8 @@
+let persons = [
+    { "fname": "Teppo", "lname": "Testi", "address": "Isokatu" },
+    { "fname": "Liisa", "lname": "Joki", "address": "Torikaut" },
+    { "fname": "Matti", "lname": "Virta", "address": "Uusikatu" },
+];
+
+console.log("Ensimmäisen henkilön tiedot");
+console.log("etunimi: " + persons[0].fname + " sukunimi: " + persons[0].lname + " osoite: " + persons[0].address);
