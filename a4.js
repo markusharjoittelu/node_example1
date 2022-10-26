8e72ff4bc8ae36b4ae35f2d1a7fafddc48c96a52
Markus <niiranen.markus@gmail.com>
2022-10-26 10:56:39 +0300

    a4 valmis

---
+++ a4.js
@@ -0,0 +1,15 @@
+let cars = ['Volvo', 'Nissan', 'Audi'];
+
+console.log("1. auto on = " + cars[0]);
+console.log("2. auto on = " + cars[1]);
+console.log("3. auto on = " + cars[2]);
+
+console.log("Tulostus for-loopilla");
+for (let x = 0; x <= 2; x++) {
+    console.log(x + 1 + ". auto on = " + cars[x]);
+}
+
+console.log("Tulostus foreach-loopilla");
+cars.forEach(element => {
+    console.log("Auto = " + element);
+});
